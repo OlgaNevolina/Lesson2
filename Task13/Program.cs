606964f214411e93df771eac65ea28b32af41709
nevol78@mail.ru <nevol78@mail.ru>
2022-05-17 23:27:54 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите целое число: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Третья цифра числа: ");
+Console.WriteLine(num > 99 ? num.ToString()[2] : "Третьей цифры нет");
